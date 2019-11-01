3abd9f31b876d658b3663f2cd24f03cb0286441c
Akbar <akbarali1klr@gmail.com>
2019-11-01 13:24:25 +0530

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
   const handleSubmit = (e) => {
       if (persons.find(x =>x.name===newName)) {
         alert(`${newName} is already added to the phonebook`);
         e.preventDefault();
         return false;
       }
-      setPersons([...persons, {name:newName}]);
+      setPersons([...persons, {name:newName, number:newNumber}]);
       setNewName('');
+      setNewNumber('');
       e.preventDefault();
   }
   return (
@@ -21,13 +24,17 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={(e)=>{setNewName(e.target.value.trim());}}/>
+      </div>
+      <div>
+          number: <input value={newNumber} onChange={(e)=>{setNewNumber(e.target.value.trim());}}/>
+
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(x=>(<h3>{x.name}</h3>))}</div>
+      <div>{persons.map(x=>(<h3>{x.name} {x.number}</h3>))}</div>
     </div>
   )
 }
