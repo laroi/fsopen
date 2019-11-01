9d23d6f85063fe4332f511e35c530d2abf085449
Akbar <akbarali1klr@gmail.com>
2019-11-01 14:04:03 +0530

    2.9*: The Phonebook Step4 & 2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-
+  const [ filter, setFilter ] = useState('');
   const handleSubmit = (e) => {
       if (persons.find(x =>x.name===newName)) {
         alert(`${newName} is already added to the phonebook`);
@@ -18,23 +18,50 @@ const App = () => {
       setNewNumber('');
       e.preventDefault();
   }
+
+  const Filter = (props) => {
+      return (
+        <div>
+          Filter shown with: <input autofocus value={filter} onChange={(e)=> {setFilter(e.target.value);}}/>
+        </div>
+      )
+  };
+  const PersonForm = (props) => {
+      return (
+          <div>
+          <h1>Add New</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              name: <input value={newName} onChange={(e)=>{setNewName(e.target.value.trim());}}/>
+          </div>
+          <div>
+              number: <input value={newNumber} onChange={(e)=>{setNewNumber(e.target.value.trim());}}/>
+
+            </div>
+            <div>
+              <button type="submit">add</button>
+            </div>
+          </form>
+          </div>
+      )
+  };
+  const Numbers = (props) => {
+      let src = [...persons];
+      if (filter) {
+        src = persons.filter(x=>x.name.toLowerCase().includes(filter.toLowerCase()))
+      }
+      return (
+          <div>{src.map(x=>(<h3>{x.name} {x.number}</h3>))}</div>
+      )
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={(e)=>{setNewName(e.target.value.trim());}}/>
-      </div>
-      <div>
-          number: <input value={newNumber} onChange={(e)=>{setNewNumber(e.target.value.trim());}}/>
-
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter/>
+      <PersonForm/> 
       <h2>Numbers</h2>
-      <div>{persons.map(x=>(<h3>{x.name} {x.number}</h3>))}</div>
+      <Numbers/> 
     </div>
   )
 }
